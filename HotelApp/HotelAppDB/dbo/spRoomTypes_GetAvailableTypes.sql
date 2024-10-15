1e03ffa96b7ee2be3a719800939a3653cb924f24
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-15 13:18:40 +0200

    sql: Storage procedure for getting available types of bookable rooms

---
+++ HotelApp/HotelAppDB/dbo/spRoomTypes_GetAvailableTypes.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[spRoomTypes_GetAvailableTypes]
+	@startDate date,
+	@endDate date
+AS
+begin
+set nocount on;
+
+	select t.Id, t.Title, t.Description, T.Price
+	from dbo.Rooms r
+	inner join dbo.RoomTypes t on t.Id = r.RoomTypeId
+	where r.Id not in (
+	select b.RoomId
+	from dbo.Bookings b
+	where (@startDate >= b.StartDate and @startDate < b.EndDate)
+	or (@endDate >= b.StartDate and @endDate <= b.EndDate)
+	or(b.StartDate >= @startDate and b.StartDate <= @endDate)
+	)
+	group by t.Id, t.Title, t.Description, T.Price;
+end
