dac1c266b9364bbb841557972e7af17917bf740e
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-16 15:42:12 +0200

    sql: Stored procedure for getting booking information by last name and todays date

---
+++ HotelApp/HotelAppDB/dbo/Stored Procedures/spBookings_Search.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_Search]
+	@lastName nvarchar(50),
+	@startDate date
+AS
+begin
+
+	set nocount on;
+	select [b].[Id], [b].[RoomId], [b].[GuestId], [b].[StartDate], [b].[EndDate], [b].[CheckedIn], [b].[TotalCost], 
+	[g].[FirstName], [g].[LastName], 
+	[r].[RoomNumber], [r].[RoomTypeId], 
+	[rt].[Title], [rt].[Description], [rt].[Price]
+	from dbo.Bookings b
+	inner join dbo.Guests g on g.id = b.GuestId
+	inner join dbo.Rooms r on r.id = b.RoomId
+	inner join dbo.RoomTypes rt on rt.id = r.RoomTypeId
+	where b.StartDate = @startDate and g.LastName = @lastName
+
+end
