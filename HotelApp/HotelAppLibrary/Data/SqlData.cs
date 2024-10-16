602832b30c04a463969bf4b203c0d110bcb2475f
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-16 13:59:39 +0200

    func: public void BookGuest books a room for a guest, by inserting relevant info inside db table Bookings

--- HotelApp/HotelAppLibrary/Data/SqlData.cs
+++ HotelApp/HotelAppLibrary/Data/SqlData.cs
@@ -21,4 +21,35 @@ public class SqlData
         return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes",
                                                     new { startDate, endDate }, connectionStringName, true);
     }
+    public void BookGuest(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId)
+    {
+        GuestModel guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuests_Insert",
+                                                             new { firstName, lastName },
+                                                             connectionStringName,
+                                                             true).First();
+
+        RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypes where Id = @Id",
+                                                                        new { Id = roomTypeId },
+                                                                        connectionStringName,
+                                                                        false).First();
+
+        TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
+
+        List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms",
+                                                                          new { startDate, endDate, roomTypeId },
+                                                                          connectionStringName,
+                                                                          true);
+
+        _db.SavedData("dbo.spBookings_Insert",
+                      new
+                      {
+                          roomId = availableRooms.First().Id,
+                          guestId = guest.Id,
+                          startDate = startDate,
+                          endDate = endDate,
+                          totalCost = timeStaying.Days * roomType.Price
+                      },
+                      connectionStringName,
+                      true);
+    }
 }
