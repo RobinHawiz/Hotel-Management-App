6339444daddf2e7e3e6f0dc934d3fe6e67143595
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-18 12:50:34 +0200

    file: Code-behind setup for CheckInForm.xaml page

---
+++ HotelApp/HotelApp.Desktop/CheckInForm.xaml.cs
@@ -0,0 +1,55 @@
+﻿using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for CheckInForm.xaml
+    /// </summary>
+    public partial class CheckInForm : Window
+    {
+        private readonly IDatabaseData _db;
+        private BookingFullModel _data = null;
+        public CheckInForm(IDatabaseData db)
+        {
+
+            InitializeComponent();
+            _db = db;
+        }
+
+        public void PopulateCheckInInfo(BookingFullModel data)
+        {
+            _data = data;
+
+            CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
+
+            firstNameText.Text = _data.FirstName;
+            lastNameText.Text = _data.LastName;
+            titleText.Text = _data.Title;
+            roomNumberText.Text = _data.RoomNumber;
+            totalCostText.Text = _data.TotalCost.ToString("C", CultureInfo.CurrentCulture);
+
+        }
+
+        private void checkInUser_Click(object sender, RoutedEventArgs e)
+        {
+            _db.CheckInGuest(_data.Id);
+            Close();
+        }
+    }
+}
